10d1d3e8aea7dca1a23f71ee8b4f04f1a0fee072
DeGraciaMathieu <degracia.mathieu@gmail.com>
2020-12-23 17:26:53 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,8 @@ const IndexPage = () => {
           <div className="space-y-12 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
             <div className="space-y-5 sm:space-y-4">
               <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">À propos de nous</h2>
-              <p className="text-xl text-gray-300">Plus de 15 ans d'expériences en développement mit à profit d'applications Web modernes propulsées par Laravel / VueJS / Tailwind.</p>
+              <p className="text-xl text-gray-300">Plus de 15 ans d'expériences en développement mit à profit d'applications Web modernes propulsées par Laravel / VueJS / TailwindCSS.</p>
+              <p class="text-xl text-gray-300">Nous offrons une expertise en gestion &amp; développement de projets, déploiement & tests automatisés, analyse de code, UI/UX, performance & accessibilité.</p>
             </div>
             <div className="lg:col-span-2">
               <ul className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 mx-auto lg:gap-8">
