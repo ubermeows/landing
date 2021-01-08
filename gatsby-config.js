ce5479ccf6a0319bbbdac34560e74b3c41e6aa0a
marionj <marionj@focus-home.com>
2021-01-08 10:33:51 +0100

    gtag and clean

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,9 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        trackingId: "G-F823NYDPRN",
+        id: "G-F823NYDPRN",
       },
     },
     {
@@ -33,16 +33,6 @@ module.exports = {
         crossOrigin: `use-credentials`,
       },
     },
-    "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
   ],
 };
