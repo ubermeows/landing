801fff0c873df1ebd3ef3330e74ea80939451a3d
marionj <marionj@focus-home.com>
2021-01-08 10:19:47 +0100

    dynamic year footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ const IndexPage = () => {
       <footer className="bg-gray-900 border-t border-gray-800">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
           <div class="mt-8 md:mt-0 md:order-1">
-            <p class="text-center text-base text-gray-300">&copy; 2020 Übermeows</p>
+            <p class="text-center text-base text-gray-300">&copy; {new Date().getFullYear()} Übermeows</p>
           </div>
         </div>
       </footer>
